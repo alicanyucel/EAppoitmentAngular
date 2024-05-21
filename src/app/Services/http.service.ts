7cbf5069695abc52f9b35b6a9fc2a00fbcd08c97
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-05-21 08:53:29 +0300

    akşam devam edilsin ön yüz temel tsarımları yapılsın.

--- src/app/Services/http.service.ts
+++ src/app/Services/http.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,15 @@ import { Injectable } from '@angular/core';
 })
 export class HttpService {
 
-  constructor() { }
+  constructor(
+    private http:HttpClient
+  ) { }
+  post(apiUrl:string){
+    this.http.post(``,body).subscribe({
+      next:(res=>{
+
+      }),
+      error:()
+    })
+  }
 }
