894167412b5c00c1371a5a41ba67809b633d5d71
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-06-09 11:48:24 +0300

    error service

--- src/app/Services/swal.service.ts
+++ src/app/Services/swal.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
+import { title } from 'process';
 import { timer } from 'rxjs';
+import { text } from 'stream/consumers';
 import Swal from 'sweetalert2';
 
 @Injectable({
@@ -12,11 +14,30 @@ export class SwalService {
 Swal.fire({
   title:title,
   timer:3000,
+  text:'Basarılı',
   icon:icon,
   toast:true,
+ showConfirmButton:false,
+ position:'bottom-right',
  showCancelButton:false,
  showCloseButton:false
-})
-  }
+});
+}
+calSwal(title:string,text:string,callBack:()=>void)
+{
+  Swal.fire({
+    title:title,
+    text:text,
+    icon:"question",
+    showConfirmButton:true,
+    confirmButtonText:"Delete",
+    showCancelButton:true,
+    cancelButtonText:"Cancel"}).then(res=>{
+      if(res.isConfirmed)
+        {
+          callBack();
+        }
+  });
+}
 }
 export type SwetalertIcon='success' | 'error' | 'warning'| 'info' | 'question'
