8eb43f4c793e5081a5a623e84443ad643f606884
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-06-09 12:32:15 +0300

    error service success

--- src/app/Services/error.service.ts
+++ src/app/Services/error.service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from '@angular/core';
+import { ErrorHandler, Injectable } from '@angular/core';
+import { SwalService } from './swal.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ErrorService {
+  
+  constructor(private swal:SwalService) { }
 
-  constructor() { }
+errorHandler(err:HttpErrorResponse){
+console.log(err);
+let message="Error1";
+if(err.status===0)
+  {
+  message="api is not available";
+  }
+  else if(err.status===404){
+    message="api not found";
+ }
+ else if(err.status===500){
+  for(const e of err.error.errorMessages){
+    message+= e +'\n';
+  }
+ }
+ this.swal.callToast(message,"error");
+}
 }
--- src/app/Services/generic.service.ts
+++ src/app/Services/generic.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ResultModel } from '../Models/result.model';
 import { api } from '../Constans/constans';
+import { ErrorService } from './error.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,13 +10,14 @@ import { api } from '../Constans/constans';
 export class GenericService {
 
   constructor(
-    private http: HttpClient) { }
+    private http: HttpClient,private error:ErrorService) { }
     post<T>(apiUrl: string, body: any, callBack: (res: ResultModel<T>) => void, errorCallback?: (err: HttpErrorResponse) => void) {
       this.http.post<ResultModel<T>>(`${api}/${apiUrl}`, body).subscribe({
         next: (res => {
           callBack(res);
         }),
         error: (err: HttpErrorResponse) => {
+          this.error.errorHandler(err);
           if (errorCallback!==undefined) {
             errorCallback(err);
           }
