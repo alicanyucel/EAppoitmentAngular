7236b849bb4dee1cdd7212ca66606f30ed24ace1
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-06-09 12:32:38 +0300

    format document

--- src/app/Services/error.service.ts
+++ src/app/Services/error.service.ts
@@ -6,24 +6,23 @@ import { HttpErrorResponse } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ErrorService {
-  
-  constructor(private swal:SwalService) { }
 
-errorHandler(err:HttpErrorResponse){
-console.log(err);
-let message="Error1";
-if(err.status===0)
-  {
-  message="api is not available";
+  constructor(private swal: SwalService) { }
+
+  errorHandler(err: HttpErrorResponse) {
+    console.log(err);
+    let message = "Error1";
+    if (err.status === 0) {
+      message = "api is not available";
+    }
+    else if (err.status === 404) {
+      message = "api not found";
+    }
+    else if (err.status === 500) {
+      for (const e of err.error.errorMessages) {
+        message += e + '\n';
+      }
+    }
+    this.swal.callToast(message, "error");
   }
-  else if(err.status===404){
-    message="api not found";
- }
- else if(err.status===500){
-  for(const e of err.error.errorMessages){
-    message+= e +'\n';
-  }
- }
- this.swal.callToast(message,"error");
-}
 }
