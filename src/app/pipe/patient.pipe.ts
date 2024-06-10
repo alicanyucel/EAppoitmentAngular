bf917ed8edcc5e4b2aa2792abef0c713fcf9fb7e
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-06-10 18:29:05 +0300

    patient sayfası tasarlandı.

---
+++ src/app/pipe/patient.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { PatientModel } from '../Models/patient.model';
+
+@Pipe({
+  name: 'patient',
+  standalone: true
+})
+export class PatientPipe implements PipeTransform {
+
+  transform(value: PatientModel[],search:string): PatientModel[]{
+    if(!search) {
+      return value;
+    }
+    return value.filter(p=>
+      p.fullName.toLocaleLowerCase().includes(search.toLocaleLowerCase()) ||
+      p.fullAdress.toLocaleLowerCase().includes(search.toLocaleLowerCase())||
+      p.city.toLocaleLowerCase().includes(search.toLocaleLowerCase())||
+      p.town.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+    )
+  }
+}
